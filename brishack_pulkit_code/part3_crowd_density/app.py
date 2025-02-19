ddb8b530d0e334e356a09b6ba7c4de5eb49c2913
Pulkit Dhingra <pulkit12dhingra@gmail.com>
2025-02-19 12:05:09 +0000

    Uploading the final code

--- brishack_pulkit_code/part3_crowd_density/app.py
+++ brishack_pulkit_code/part3_crowd_density/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, jsonify
 import random
+import pandas as pd
 
 app = Flask(__name__)
 
 # Predefined circle positions
-circle_positions = [[590, 600], [250, 370], [1100, 550], [210, 120], [0, 0]]
+circle_positions = [[590, 800], [350, 400], [1100, 550], [210, 120], [0, 0]]
 
 @app.route("/")
 def home():
@@ -12,13 +13,18 @@ def home():
 
 @app.route("/get_sizes")
 def get_sizes():
+
+    data = pd.read_csv("people_count.csv")
+    get_size_latest = data.shape[0]
+    size_one = int(data.iloc[get_size_latest-1, 1])
+
     # Generate random sizes (0-40)
-    circle_sizes = [random.randint(0, 40) for _ in range(len(circle_positions))]
+    circle_sizes = [0,size_one+10,0,0,0]
     
     # Determine colors based on size (Red if >30, else Green)
-    circle_colors = ["green" if size <= 30 else "red" for size in circle_sizes]
+    circle_colors = ["green" if size <= 20 else "red" for size in circle_sizes]
 
     return jsonify(sizes=circle_sizes, colors=circle_colors)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000,debug=True)
